ffd7db2307adf1ba01a19abd0e365a311ca585df
Anjuka <anjukako@gmail.com>
2019-12-08 19:14:58 +0530

    Get Current Location & Camera move

--- app/src/main/java/com/anjukakoralage/muveassigment/ui/booking/BookingFragment.java
+++ app/src/main/java/com/anjukakoralage/muveassigment/ui/booking/BookingFragment.java
@@ -1,19 +1,46 @@
 package com.anjukakoralage.muveassigment.ui.booking;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.anjukakoralage.muveassigment.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 /**
  * Created by  on 07,December,2019
  */
-public class BookingFragment extends Fragment {
+public class BookingFragment extends Fragment implements OnMapReadyCallback {
+
+    private static final String TAG = "BookingFragment";
+    private static String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static String COARES_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static int LOCATION_PERMISSION_CODE = 1234;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private boolean locationPermissionGranted = false;
+    GoogleMap gMap;
 
     public BookingFragment() {
     }
@@ -22,6 +49,105 @@ public class BookingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+        View view = inflater.inflate(R.layout.fragment_booking, container, false);
+        getLocationPermission();
+
+        return view;
+    }
+
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (locationPermissionGranted){
+            getDeviceLocation();
+        }
+    }
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getContext(),
+                    COARES_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationPermissionGranted = true;
+                initMap();
+            }else{
+                ActivityCompat.requestPermissions(this.getActivity(),
+                        permissions,
+                        LOCATION_PERMISSION_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    permissions,
+                    LOCATION_PERMISSION_CODE);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+
+        switch (requestCode){
+            case 123:{
+                if (grantResults.length > 0){
+                    for (int i = 0; i < grantResults.length; i++){
+                        if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                            locationPermissionGranted = false;
+                            break;
+                        }
+                    }
+                    locationPermissionGranted = true;
+                    //initMap
+                    initMap();
+                }
+            }
+
+        }
+    }
+
+    private void getDeviceLocation(){
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        try{
+            if (locationPermissionGranted){
+                Task location = fusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "onComplete: Found Location");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 16);
+                        }
+                        else {
+                            Log.d(TAG, "onComplete: Not Found Location");
+                            //toast to show
+                        }
+                    }
+                });
+            }
+
+        } catch (SecurityException e){
+            Log.d(TAG, "getDeviceLocation: " + e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: Move Camera to " + latLng.latitude + " - " + latLng.longitude);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom ));
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
     }
 }
--- app/src/main/java/com/anjukakoralage/muveassigment/ui/main/MainActivity.java
+++ app/src/main/java/com/anjukakoralage/muveassigment/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.anjukakoralage.muveassigment.ui.main;
 
+import android.app.Dialog;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -9,13 +10,17 @@ import android.widget.FrameLayout;
 
 import com.anjukakoralage.muveassigment.R;
 import com.anjukakoralage.muveassigment.ui.splash.SplashFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.MapView;
 
 import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     public static FragmentManager fragmentManager;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,27 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.add(R.id.fragment_container, splashFragment, null);
             fragmentTransaction.commit();
         }
+
+    }
+
+    public boolean isServiceOK(){
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (available == ConnectionResult.SUCCESS){
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(this,available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            //cannot make map request
+        }
+        return false;
+    }
+
+    private void init(){
 
     }
 }
